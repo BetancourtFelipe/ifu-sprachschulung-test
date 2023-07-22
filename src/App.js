bf3dcd02ebc459c09e013a1a0c3af0ae5c4e8069
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2023-07-22 09:30:01 +0200

    Start project ifu

---
+++ src/App.js
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section>
+          <div>
+            <div>info tel usw</div>
+          </div>
+        </section>
+        <div>
+          <div>
+            <nav>
+              <a>IFU</a>
+              <a>Kontakt</a>
+            </nav>
+          </div>
+        </div>
+        <main>
+          <div>
+            <div>MAIN</div>
+          </div>
+        </main>
+      </header>
+      <footer>footer</footer>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { css, Global } from '@emotion/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          font-family: sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+        *,
+        ::after,
+        ::before {
+          box-sizing: border-box;
+        }
+      `}
+    />
+    <App />
+  </React.StrictMode>,
+);
